4517e25513813cda7f67f0491712cc477a860527
sllorens-cuenca <sllorens.cuenca@gmail.com>
2025-03-04 17:22:26 +0100

    Add the habit routes

---
+++ routes/habit.routes.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+
+const { isAuthenticated, isOwner } = require("../middleware/jwt.middleware");
+
+const Habit = require("../models/Habit.model")
+
+// POST new habit /api/habit
+router.post("/habit", isAuthenticated, (req, res) => {
+    Habit.create(req.body)
+    .then((newHabit) => {
+        res.status(201).json(newHabit)
+    })
+    .catch(error => {
+        res.status(500).json({message: "Error creating a new habit"})
+    })
+})
+
+// GET all habits /api/habit
+router.get("/habit", isAuthenticated, (req, res) => {
+    Habit.find()
+    //.populate("goal") --- don't know if it's necessary
+    .then((habits) => {
+        res.status(200).json(habits)
+    })
+    .catch(error => {
+        res.status(500).json({message: "Error getting the habit"})
+    })
+})
+
+// GET specific habit 
+router.get("/habit/:habitId", isAuthenticated, (req, res) => {
+    const {habitId} = req.params
+
+    Habit.findById(habitId)
+    //.populate("goal") --- don't know if it's necessary
+    .then((habits) => {
+        res.status(200).json(habits)
+    })
+    .catch(error => {
+        res.status(500).json({message: "Error getting the habit"})
+    })
+})
+
+// UPDATE (PUT) habit
+router.put("/habit/:habitId", isAuthenticated, isOwner, (req, res) => {
+    const {habitId} = req.params
+
+    Habit.findByIdAndUpdate(habitId, req.body, {new: true})
+    .then((updatedHabit) => {
+        res.status(200).json(updatedHabit)
+    })
+    .catch(error => {
+        res.status(500).json({message: "Error updating the habit"})
+    })
+})
+
+// DELETE /api/habit/:habitId
+router.delete("/habit/:habitId", isAuthenticated, isOwner, (req, res) => {
+    const {habitId} = req.params
+
+    Habit.findByIdAndDelete(habitId)
+    .then(res.status(204).end())
+    .catch(error => res.status(500).json({message: "Error deleting the habit"}))
+})
+
+module.exports = router;
