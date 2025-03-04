50ae5ee84b84cd4d6e840701b07ded8be230f082
sllorens-cuenca <sllorens.cuenca@gmail.com>
2025-03-04 16:23:09 +0100

    add model for Habit

---
+++ models/Habit.model.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const habitSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "The name of the habit is required!"]
+        },
+        description: {
+            type: String
+        },
+        color: {
+            type: String,
+            default: "Choose a color!"
+        },
+        frequency: {
+            type: String,
+            enum: ["daily", "every two days", "every three days"],
+            required: true
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        reminder: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+const Habit = model("Habit", habitSchema);
+
+module.exports = Habit;
