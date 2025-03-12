952b43694a1607002a3478eaba25172e3f42056f
Julia Solias <juliasohu@gmail.com>
2025-03-12 16:18:48 +0100

    Route to check achievedCounts

--- routes/goals.routes.js
+++ routes/goals.routes.js
@@ -6,81 +6,90 @@ const isOwner = require("../middleware/isOwner.middleware");
 
 // POST new goal
 router.post("/goals", isAuthenticated, (req, res) => {
-    Goal.create(req.body)
+  Goal.create(req.body)
     .then((newGoal) => res.status(201).json(newGoal))
-    .catch(e => res.status(500).json({message: "Error"}))
-})
+    .catch((e) => res.status(500).json({ message: "Error" }));
+});
 
 //UPDATE (PATCH) goal
 
 router.patch("/goals/:goalId", isAuthenticated, isOwner, (req, res) => {
-    const {goalId} = req.params
-    Goal.findByIdAndUpdate(goalId, req.body, {new: true})
-    .populate('habits.habit')
+  const { goalId } = req.params;
+  Goal.findByIdAndUpdate(goalId, req.body, { new: true })
+    .populate("habits.habit")
     .then((updatedGoal) => res.status(200).json(updatedGoal))
-    .catch(e => res.status(500).json({message: "Error"}))
-})
+    .catch((e) => res.status(500).json({ message: "Error" }));
+});
 
 //PATCH adds one to habit achieved count
 
 router.patch("/goals/checkHabit/:goalId", async (req, res) => {
   try {
-    const {goalId} = req.params
-    const {habitId} = req.body
-    console.log(req.body)
-    const goal = await Goal.findById(goalId)
-    goal.habits.map(habit => habit.habit === habitId && habit.achievedCount + 1)
-    goal.save()
-    return res.status(200).json({msg: "should be good"})
+    const { goalId } = req.params;
+    const { goal } = req.body;
+  
+    const updated = await Goal.findByIdAndUpdate(goalId, goal, {new:true});
+    
+    return res.status(200).json({ msg: "should be good" });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json(error)
+    console.log(error);
+    return res.status(500).json(error);
   }
-})
+});
 
 //GET all goals (commented out for performance reasons)
 router.get("/goals", isAuthenticated, (req, res) => {
-    Goal.find({createdBy: req.payload._id})
-    .populate('habits.habit')
+  Goal.find({ createdBy: req.payload._id })
+    .populate("habits.habit")
     .then((goals) => {
-        const goalsWithCalculatedFields = goals.map(goal => {
-          const { requiredAchievedCount, remainingAchievedCount } = goal.calculateRequiredAchievedCount();
-          return {
-            ...goal.toObject(),  // Convert Mongoose document to plain object
-            requiredAchievedCount,
-            remainingAchievedCount
-          };
-        });
-  
-        res.status(200).json(goalsWithCalculatedFields);
-      })
-      .catch(e => res.status(500).json({ message: "Error fetching goals", error: e.message }));
-  });
+      const goalsWithCalculatedFields = goals.map((goal) => {
+        const { requiredAchievedCount, remainingAchievedCount } =
+          goal.calculateRequiredAchievedCount();
+        return {
+          ...goal.toObject(), // Convert Mongoose document to plain object
+          requiredAchievedCount,
+          remainingAchievedCount,
+        };
+      });
+
+      res.status(200).json(goalsWithCalculatedFields);
+    })
+    .catch((e) =>
+      res
+        .status(500)
+        .json({ message: "Error fetching goals", error: e.message })
+    );
+});
 
 router.get("/goals/:goalId", isAuthenticated, isOwner, (req, res) => {
-    const {goalId} = req.params
-    Goal.findById(goalId)
-    .populate('habits.habit')
+  const { goalId } = req.params;
+  Goal.findById(goalId)
+    .populate("habits.habit")
     .then((goal) => res.status(200).json(goal))
-    .catch(e => res.status(500).json({message: "Error"}))
-})
+    .catch((e) => res.status(500).json({ message: "Error" }));
+});
 
 //DELETE goal
 
 router.delete("/goals/:goalId", isAuthenticated, isOwner, (req, res) => {
-    const {goalId} = req.params
-    Goal.findByIdAndDelete(goalId)
+  const { goalId } = req.params;
+  Goal.findByIdAndDelete(goalId)
     .then(res.status(204).end())
-    .catch(e => res.status(500).json({message: "Error"}))
-})
+    .catch((e) => res.status(500).json({ message: "Error" }));
+});
 
 // PATCH to mark goal completed
-router.patch("/goals/:goalId/complete", isAuthenticated, isOwner, (req, res) => {
+router.patch(
+  "/goals/:goalId/complete",
+  isAuthenticated,
+  isOwner,
+  (req, res) => {
     const { goalId } = req.params;
-  
+
     Goal.findByIdAndUpdate(goalId, { status: "completed" }, { new: true })
       .then((goal) => res.status(200).json(goal))
       .catch((e) => res.status(500).json({ message: "Error completing goal" }));
-  });
+  }
+);
 
 module.exports = router;
